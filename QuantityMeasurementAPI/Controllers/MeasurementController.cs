259a8f27d70426fb1919841e9a6972451e7214b1
ruchika <ruchika6698@gmail.com>
2020-06-01 15:28:18 +0530

    Added:Insert API for Conversion and save into database

--- QuantityMeasurementAPI/Controllers/MeasurementController.cs
+++ QuantityMeasurementAPI/Controllers/MeasurementController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BusinessLayer.Interface;
 using CommanLayer;
+using CommanLayer.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,36 @@ namespace QuantityMeasurementAPI.Controllers
             _config = config;
         }
 
-        
+        /// <summary>
+        /// Method to Add Conversion Detail
+        /// </summary>
+        /// <param name="Info"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("addnewentry")]
+        public IActionResult Convert([FromBody]Quantity  Info)
+        {
+            try
+            {
+                var result = BusinessLayer.Convert(Info);
+                //if entry is not equal to null
+                if (!result.Equals(null))
+                {
+                    var status = "True";
+                    var Message = "New Entry Added Sucessfully";
+                    return this.Ok(new { status, Message, data = Info });
+                }
+                else                                              //Entry is not added
+                {
+                    var status = "False";
+                    var Message = "New Entry is not Added";
+                    return this.BadRequest(new { status, Message, data = Info });
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
